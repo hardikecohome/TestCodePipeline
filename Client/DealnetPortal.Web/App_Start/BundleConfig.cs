c49f6173230930d64096c394cc1b8c71d7308ea9
Yury Yurkovskiy <yyurkovskiy@dataart.com>
2016-08-19 15:40:13 +0300

    Layout scripts added

--- Client/DealnetPortal.Web/App_Start/BundleConfig.cs
+++ Client/DealnetPortal.Web/App_Start/BundleConfig.cs
@@ -22,6 +22,8 @@ namespace DealnetPortal.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/dealnet").Include(
+                     "~/Scripts/layout.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ Client/DealnetPortal.Web/Scripts/layout.js
@@ -0,0 +1,31 @@
+﻿$(document)
+    .ready(function () {
+
+        var selected = getValueFromLocalStorage('dealnet-sidebar-selected-item', 'sidebar-item-homepage');
+        var selectedItem = $('#' + selected).addClass('dealnet-sidebar-item-selected');
+
+        var link = selectedItem.find('a').attr('href').toLowerCase();
+        var location = window.location.pathname.toLowerCase();
+        if (link && location != link.toLowerCase() && !location.indexOf('/account',0)) {
+            window.location.href = link;
+        }
+        var sidebarItems = $('.dealnet-sidebar-item');
+        sidebarItems.click(function () {
+            sidebarItems.removeClass('dealnet-sidebar-item-selected');
+            $(this).addClass('dealnet-sidebar-item-selected');
+            localStorage.setItem('dealnet-sidebar-selected-item', $(this).attr('id'));
+            window.location.href = $(this).find('a').attr('href');
+        });
+
+    });
+
+
+function getValueFromLocalStorage(key, defaultValue) {
+    var item = localStorage.getItem(key);
+    if (item) {
+        return item;
+    } else {
+        console.log('Item with key ' + key + ' not found in local storage. Default value "' + defaultValue + '" used');
+        return defaultValue;
+    }
+};
