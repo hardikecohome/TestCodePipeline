f38c42910e715add9841e128c57f9b9fbbb7b3df
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2016-08-25 16:23:21 +0300

    add html5 camera

--- Server/DealnetPortal.Api/Controllers/ScanProcessingController.cs
+++ Server/DealnetPortal.Api/Controllers/ScanProcessingController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using DealnetPortal.Api.Common.Constants;
 using DealnetPortal.Api.Integration;
+using DealnetPortal.Api.Models;
 using DealnetPortal.Api.Models.Enumeration;
 using DealnetPortal.Api.Models.Scanning;
 using DealnetPortal.Utilities;
@@ -23,16 +25,20 @@ namespace DealnetPortal.Api.Controllers
             _loggingService = loggingService;
         }
        
-        public IHttpActionResult PostScanProcessing(ScanningRequest scanningRequest)
+        public async Task<IHttpActionResult> PostScanProcessing(ScanningRequest scanningRequest)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            ImageScanManager scanManager = new ImageScanManager();
-            var result = scanManager.ReadDriverLicense(scanningRequest);
-
+            var result = await Task.Run(() =>
+            {
+                ImageScanManager scanManager = new ImageScanManager();
+                var res = scanManager.ReadDriverLicense(scanningRequest);
+                return res;
+            });
+           
             _loggingService.LogInfo("Recieved request for scan driver license");
             
             if (result != null && result.Item2.All(al => al.Type != AlertType.Error))
