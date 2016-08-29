c06585bc54421aaa55c0eedb430868ec3c586d63
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2016-08-29 18:53:03 +0300

    Add Contract stuctures to DB

---
+++ Server/DealnetPortal.Domain/Enums/ContractState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealnetPortal.Domain.Enums
+{
+    /// <summary>
+    /// A current state of contract
+    /// </summary>
+    public enum ContractState
+    {
+        [Description("Started to fill new contract")]
+        Started = 0,
+        [Description("Contract in progress, some data have been filled")]
+        InProgress = 1,
+        [Description("Contract filled and sent to Aspire")]
+        Completed = 2
+    }
+}
