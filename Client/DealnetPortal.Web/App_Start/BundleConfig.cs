d89255b59cb36632fb23eab4fb06af2830e0ce78
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2016-09-01 16:19:35 +0300

    fix BundleConfig

--- Client/DealnetPortal.Web/App_Start/BundleConfig.cs
+++ Client/DealnetPortal.Web/App_Start/BundleConfig.cs
@@ -25,10 +25,9 @@ namespace DealnetPortal.Web
             bundles.Add(new ScriptBundle("~/bundles/libs").Include(
                      "~/Scripts/datatables/jquery.dataTables.js", "~/Scripts/chart.js"));
             bundles.Add(new ScriptBundle("~/bundles/dealnet").Include(
-                     "~/Scripts/layout.js"));
-            bundles.Add(new ScriptBundle("~/bundles/basic-info").IncludeDirectory(
-                     "~/Scripts/BasicInfo", "*.js"));
                      "~/Scripts/layout.js", "~/Scripts/home-page.js"));
+            bundles.Add(new ScriptBundle("~/bundles/basic-info").IncludeDirectory(
+                     "~/Scripts/BasicInfo", "*.js"));                    
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
