d60f86c0a8eca181cc2c413ff167827fb2314130
Yury Yurkovskiy <yyurkovskiy@dataart.com>
2016-09-01 10:34:20 +0300

    Completed

--- Client/DealnetPortal.Web/Controllers/ReportsController.cs
+++ Client/DealnetPortal.Web/Controllers/ReportsController.cs
@@ -6,22 +6,76 @@ using System.Net.Http;
 
 namespace DealnetPortal.Web.Controllers
 {
+    using System.Globalization;
     using System.Threading.Tasks;
     using System.Web.Mvc;
 
     public class ReportsController : Controller
     {
         [HttpGet]
-        public async Task<ActionResult> WorkItems()
+        public ActionResult WorkItems()
         {
             return this.Json(new[]
             {
 
-                new[] {"1", "1", "1", "1", "1", "1", "1"},
-                new[] {"2", "2", "2", "2", "2", "2", "2"},
-                new[] {"3", "3", "3", "3", "3", "3", "3"}
+                new[] { "1654", "User1", "Waiting on Documents","1654", "user1@email.com", "555-555-1234", "2015/10/01"},
+                new[] { "1655", "User2", "Waiting on Documents","1655", "user2@email.com", "555-555-1234", "2015/10/01"},
+                new[] { "1655", "User3", "Waiting on Documents","1656", "user3@email.com", "555-555-1234", "2015/10/01"},
 
-            },JsonRequestBehavior.AllowGet);
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+
+        public ActionResult DealFlowOverview(DealFlowType type)
+        {
+            var rand = new Random();
+            var labels = new List<string>();
+            var datasets = new List<object>();
+            var data = new List<int>();
+            switch (type)
+            {
+                case DealFlowType.Month:
+                    {
+                        for (int i = 1; i < DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month); i++)
+                        {
+                            data.Add(rand.Next(1, 100));
+                            labels.Add(i.ToString());
+                        }
+                        datasets.Add(new {data });
+                        break;
+                    }
+                case DealFlowType.Week:
+                    {
+                        foreach (var day in DateTimeFormatInfo.CurrentInfo.DayNames)
+                        {
+                            data.Add(rand.Next(1, 100));
+                            labels.Add(day);
+                        }
+                        datasets.Add(new {data });
+                        break;
+                    }
+                case DealFlowType.Year:
+                    {
+                        foreach (var month in DateTimeFormatInfo.CurrentInfo.MonthNames)
+                        {
+                            data.Add(rand.Next(1, 100));
+                            labels.Add(month);
+                        }
+                        datasets.Add(new {data });
+                        break;
+                    }
+            }
+
+            return this.Json(new { labels, datasets }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public enum DealFlowType
+        {
+            Week,
+            Month,
+            Year
         }
     }
 }
--- Client/DealnetPortal.Web/Scripts/home-page.js
+++ Client/DealnetPortal.Web/Scripts/home-page.js
@@ -1,6 +1,55 @@
 ﻿$(document)
     .ready(function () {
-        $.when($.ajax('/reports/WorkItems', { mode: 'GET' }))
+        showChart();
+        showTable();
+        $('.dealnet-chart-switch-button')
+            .click(function () {
+                $('.dealnet-chart-switch-button').removeClass('dealnet-chart-switch-button-selected');
+                $(this).addClass('dealnet-chart-switch-button-selected');
+                showChart();
+            });
+    });
+
+
+
+
+function showChart() {
+    $.when($.ajax('/reports/DealFlowOverview',
+                {
+                    mode: 'GET',
+                    data: {
+                        type: $('.dealnet-chart-switch-button-selected').attr('data-type')
+                    }
+                })
+                    )
+                    .done(function (data) {
+                        var canvas = document.getElementById("data-flow-overview");
+                        var chart = new Chart(canvas, {
+                            type: 'bar',
+                            data: data,
+                            options: {
+                                legend: {
+                                  display:false  
+                                },
+                                elements: {
+                                    rectangle: {
+                                        backgroundColor: 'rgba(221, 243, 213, 1)'
+                                    }
+                                },
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true
+                                        }
+                                    }]
+                                }
+                            }
+                        });
+                    });
+};
+
+function showTable() {
+    $.when($.ajax('/reports/WorkItems', { mode: 'GET' }))
     .done(function (data) {
         $('#work-items-table').DataTable({
             'data': data,
@@ -15,46 +64,4 @@
             ]
         });
     });
-        var canvas = document.getElementById("data-flow-overview");
-        var chart = new Chart(canvas, {
-            type: 'bar',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-    });
-
-
-
-
-
+};
