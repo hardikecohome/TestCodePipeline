9d7538a1c5f5421d58831d7c9ea819f8cadf900e
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2016-09-01 17:27:18 +0300

    create mapping test

---
+++ Server/DealnetPortal.Api.Tests/MappingTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DealnetPortal.Api.Tests
+{
+    [TestClass]
+    public class MappingTest
+    {
+        [TestInitialize]
+        public void Intialize()
+        {
+            DealnetPortal.Api.App_Start.AutoMapperConfig.Configure();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
