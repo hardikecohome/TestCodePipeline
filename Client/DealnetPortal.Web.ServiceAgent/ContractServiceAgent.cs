9f9c34d8f53f9328075e87197316a2beebe4cc90
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2016-09-05 18:55:22 +0300

    started ContractServiceAgent implementation

---
+++ Client/DealnetPortal.Web.ServiceAgent/ContractServiceAgent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealnetPortal.Web.Common.Api;
+
+namespace DealnetPortal.Web.ServiceAgent
+{
+    public class ContractServiceAgent : ApiBase, IContractServiceAgent
+    {
+        private const string ContractApi = "Contract";
+
+        public ContractServiceAgent(IHttpApiClient client)
+            : base(client, ContractApi)
+        {            
+        }
+    }
+}
