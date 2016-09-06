f5081b20d5fc5534e7b666eae569fa8fb954fa1c
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2016-09-06 14:35:33 +0300

    Started to work with Contract service agent

--- Client/DealnetPortal.Web.ServiceAgent/ContractServiceAgent.cs
+++ Client/DealnetPortal.Web.ServiceAgent/ContractServiceAgent.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DealnetPortal.Api.Common.Constants;
+using DealnetPortal.Api.Common.Enumeration;
+using DealnetPortal.Api.Models;
+using DealnetPortal.Api.Models.Contract;
+using DealnetPortal.Utilities;
 using DealnetPortal.Web.Common.Api;
 
 namespace DealnetPortal.Web.ServiceAgent
@@ -14,6 +19,51 @@ namespace DealnetPortal.Web.ServiceAgent
         public ContractServiceAgent(IHttpApiClient client)
             : base(client, ContractApi)
         {            
+        }
+
+        public async Task<Tuple<ContractDTO, IList<Alert>>> CreateContract()
+        {
+            var alerts = new List<Alert>();
+            try
+            {            
+                var contract = await Client.GetAsync<ContractDTO>($"{_fullUri}/CreateContract");
+                return new Tuple<ContractDTO, IList<Alert>>(contract, alerts);
+            }
+            catch (Exception ex)
+            {
+                alerts.Add(new Alert()
+                {
+                    Type = AlertType.Error,
+                    Header = ErrorConstants.ContractCreateFailed,
+                    Message = ex.Message
+                });
+            }
+            return new Tuple<ContractDTO, IList<Alert>>(null, alerts);
+        }
+
+        public Task<Tuple<ContractDTO, IList<Alert>>> GetContract(int contractId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<ContractDTO>> GetContracts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Alert>> UpdateContractClientData(int contractId, IList<ContractAddressDTO> addresses, IList<CustomerDTO> customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Alert>> InitiateCreditCheck(int contractId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tuple<CreditCheckDTO, IList<Alert>>> GetCreditCheckResult(int contractId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Client/DealnetPortal.Web.ServiceAgent/IContractServiceAgent.cs
+++ Client/DealnetPortal.Web.ServiceAgent/IContractServiceAgent.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DealnetPortal.Api.Models;
+using DealnetPortal.Api.Models.Contract;
 
 namespace DealnetPortal.Web.ServiceAgent
 {
+    /// <summary>
+    /// Service agent for communicate with server-side service and controller for processing contracts (deals)
+    /// </summary>
     public interface IContractServiceAgent
     {
+        /// <summary>
+        /// Create a new contract (deal) by current logged in user (dealer)
+        /// </summary>
+        /// <returns>A new contract record with a new contract Id and alert list in a tuple</returns>
+        Task<Tuple<ContractDTO, IList<Alert>>> CreateContract();
+
+        /// <summary>
+        /// Get contract record by container id
+        /// </summary>
+        /// <param name="contractId">Container id</param>
+        /// <returns>A  contract record with and alert list in a tuple</returns>
+        Task<Tuple<ContractDTO, IList<Alert>>> GetContract(int contractId);
+
+        /// <summary>
+        /// Get contracts (deals) list for a current logged in user
+        /// </summary>
+        /// <returns>List of contracts</returns>
+        Task<IList<ContractDTO>> GetContracts();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contractId"></param>
+        /// <param name="addresses"></param>
+        /// <param name="customers"></param>
+        /// <returns></returns>
+        Task<IList<Alert>> UpdateContractClientData(int contractId, IList<ContractAddressDTO> addresses,
+            IList<CustomerDTO> customers);
+
+        /// <summary>
+        /// Initiate credit check for contract
+        /// </summary>
+        /// <param name="contractId">Contract Id</param>
+        /// <returns>List of alerts (warnings, errors)</returns>
+        Task<IList<Alert>> InitiateCreditCheck(int contractId);
+
+        /// <summary>
+        /// Get credit check results for contract
+        /// </summary>
+        /// <param name="contractId">Contract Id</param>
+        /// <returns>Credit check result and list of alerts in a tuple</returns>
+        Task<Tuple<CreditCheckDTO, IList<Alert>>> GetCreditCheckResult(int contractId);
     }
 }
