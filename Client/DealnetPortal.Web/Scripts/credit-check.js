0f54df196bebe0235b6a23ebab0e259d66e5f71f
oblonskaya <Oksana.Blonskaya@dataart.com>
2016-09-26 12:29:23 +0300

    dev-DEAL-168: New Rental, Equipment Information (fixed work of popups for step 2) [0.0.11]

--- Client/DealnetPortal.Web/Scripts/credit-check.js
+++ Client/DealnetPortal.Web/Scripts/credit-check.js
@@ -9,10 +9,8 @@ $(document)
             })
             .on('hidden.bs.modal',
                 function() {
-                  oldParent.html($('.modal-body .dealnet-credit-check-section').
-                  detach());
-                   /* $('.modal-body .dealnet-credit-check-section').
-                        detach().appendTo(oldParent);*/
+                    $('.modal-body .dealnet-credit-check-section').
+                        detach().appendTo(oldParent);
                     $('.dealnet-credit-check-section input').removeClass('form-control dealnet-input');
                     $('.dealnet-credit-check-section').removeClass('dealnet-modal-section');
                     $('.dealnet-section-title').show();
@@ -23,7 +21,6 @@ $(document)
                 });
         $('input[type="text"]').attr('readonly', 'readonly');
         $('input[type="text"]').addClass('dealnet-disabled-input');
-
 
         $("#birth-date").datepicker({
             dateFormat: 'mm/dd/yy',
@@ -84,10 +81,7 @@ $(document)
 function editData(elem) {
     var section = $(elem).parents('.dealnet-credit-check-section');
     oldParent = section.parent();
-    tempSection = section.clone();
     section.detach().appendTo('.modal-body');
-    oldParent.html(tempSection);
-    tempSection.find('input').attr('id', '');
     section.find('input').addClass('form-control dealnet-input');
     $('.modal-title').text(section.find('.dealnet-section-title').text());
     section.find('.dealnet-section-title').hide();
@@ -100,7 +94,7 @@ function editData(elem) {
         $(elem).attr('default-value', $(elem).val());
     });
    
-    //$('.dealnet-credit-check-section a').hide();
+    $('.dealnet-credit-check-section a').hide();
     $('#editor-modal').modal();
 };
 
