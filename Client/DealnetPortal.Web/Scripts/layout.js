9d9db1472fc50fb64421d547fc07518906f302d3
oblonskaya <Oksana.Blonskaya@dataart.com>
2016-09-28 11:41:33 +0300

    dev-DEAL-285 : Fixed dropdowns for datepicker and select  for ie  [0.0.3]

--- Client/DealnetPortal.Web/Scripts/layout.js
+++ Client/DealnetPortal.Web/Scripts/layout.js
@@ -1,8 +1,12 @@
 ﻿$(document)
     .ready(function () {
+      if(detectIE()){
+        $('body').addClass('ie');
+      }
         fixMetaViewportIos();
 
         $("input, textarea").placeholder();
+
         $('.dealnet-sidebar-item a[href="' + window.location.pathname + '"]')
             .parents('.dealnet-sidebar-item')
             .addClass('dealnet-sidebar-item-selected');
@@ -96,14 +100,17 @@ function customizeSelect(){
   setTimeout(function(){
     $('select').each(function(){
       if(!$(this).parents(".ui-datepicker").length){
-        $(this).wrap('<div class="custom-select">').after('<span class="caret">');
+        $(this).wrap('<div class="custom-select">');
+        if(detectIE() === false){
+          $(this).after('<span class="caret">');
+        }
       }
     });
   }, 1000);
 }
 
 function addIconsToFields(fields){
-  var fields = fields || $('.control-group input, .control-group textarea');
+  var fields = fields || $('.control-group input:not(".dealnet-disabled-input"), .control-group textarea');
   var fieldParent = fields.parent('.control-group:not(.date-group):not(.control-group-pass)');
   var fieldDateParent = fields.parent('.control-group.date-group');
   var fieldPassParent = fields.parent('.control-group.control-group-pass');
@@ -140,3 +147,47 @@ function toggleClearInputIcon(fields){
   });
 
 }
+
+  /**
+   * detect IE
+   * returns version of IE or false, if browser is not Internet Explorer
+   */
+  function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    // Test values; Uncomment to check result …
+
+    // IE 10
+    // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+    // IE 11
+    // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+    // Edge 12 (Spartan)
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+    // Edge 13
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+      // IE 10 or older => return version number
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+      // IE 11 => return version number
+      var rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+      // Edge (IE 12+) => return version number
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+  }
