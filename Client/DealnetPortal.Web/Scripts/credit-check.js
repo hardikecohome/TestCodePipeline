ca817754d70ec07354c89b79dcfbe5f168fb33f2
Denis Tish <dtish@dataart.com>
2016-09-29 05:14:39 +0300

    Credit check "...agrees" logic

--- Client/DealnetPortal.Web/Scripts/credit-check.js
+++ Client/DealnetPortal.Web/Scripts/credit-check.js
@@ -21,6 +21,18 @@
             },
             "Please enter a valid date!"
         );
+        $("#check-credit-button").click(function (event) {
+            var check1 = document.getElementById('home-owner-agrees');
+            var check2 = document.getElementById('additional1-agrees');
+            var check3 = document.getElementById('additional2-agrees');
+            var check4 = document.getElementById('additional3-agrees');
+            if (!(check1.checked &&
+                (check2 == null || check2.checked) &&
+                (check3 == null || check3.checked) &&
+                (check4 == null || check4.checked))) {
+                event.preventDefault();
+            }
+        });
     });
 
 function assignDatepicker(input) {
