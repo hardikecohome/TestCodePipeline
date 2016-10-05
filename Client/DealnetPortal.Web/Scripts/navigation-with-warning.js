7cff1a89153923fc8f6d5d745b4b3276b33dcf63
oblonskaya <Oksana.Blonskaya@dataart.com>
2016-10-05 19:17:46 +0300

    dev-DEAL-327: Modal dialog windows layout [0.0.1]

--- Client/DealnetPortal.Web/Scripts/navigation-with-warning.js
+++ Client/DealnetPortal.Web/Scripts/navigation-with-warning.js
@@ -1,6 +1,15 @@
 ﻿function navigateWithWarning(event) {
     console.log(event);
-    if (!confirm('Are you sure you want to navigate to previous steps? You will have to pass Credit Check step again.')) {
+    var url = event.target.href;
+    var message = "Are you sure you want to navigate to previous steps? You will have to pass Credit Check step again."
+    $('#alertModal').find('.modal-body p').html(message);
+    $('#alertModal').find('.modal-title').html('<svg aria-hidden="true" class="icon icon-reject"><use xlink:href="/client/Content/images/sprite/sprite.svg#icon-reject"></use></svg> Warning');
+    $('#alertModal').modal('show');
+    $('#confirmAlert').on('click', function(){
+        window.location.href = url;
+    });
+    event.preventDefault();
+    /*if (!confirm('Are you sure you want to navigate to previous steps? You will have to pass Credit Check step again.')) {
         event.preventDefault();
-    }
+    }*/
 }
