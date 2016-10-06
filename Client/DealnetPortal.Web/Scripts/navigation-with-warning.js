20cc40e5cf316f1530e538698c4eb7be5df657a0
oblonskaya <Oksana.Blonskaya@dataart.com>
2016-10-06 17:19:29 +0300

    dev-DEAL-327: Modal dialog windows layout (IE9,10,11: Modal dialog does not appear when user tries to get to screen 1/2 from 3/4/5)[0.0.4]

--- Client/DealnetPortal.Web/Scripts/navigation-with-warning.js
+++ Client/DealnetPortal.Web/Scripts/navigation-with-warning.js
@@ -1,5 +1,6 @@
 ﻿function navigateWithWarning(event) {
-    console.log(event);
+    //console.log(event); /*This part of code allow user go to href url and the code below doesn't work in IE browsers*/
+
     var url = event.target.href;
     var message = "If you change Home Owner Information you will have to pass Credit Check step again"
     $('#alertModal').find('.modal-body p').html(message);
@@ -9,6 +10,7 @@
     $('#confirmAlert').on('click', function(){
         window.location.href = url;
     });
+
     event.preventDefault();
     /*if (!confirm('Are you sure you want to navigate to previous steps? You will have to pass Credit Check step again.')) {
         event.preventDefault();
