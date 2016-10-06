970d5f4544c07ae29fad929c6688a93546c6970f
oblonskaya <Oksana.Blonskaya@dataart.com>
2016-10-06 17:10:03 +0300

    dev-DEAL-327: Modal dialog windows layout [0.0.3]

--- Client/DealnetPortal.Web/Scripts/navigation-with-warning.js
+++ Client/DealnetPortal.Web/Scripts/navigation-with-warning.js
@@ -1,9 +1,10 @@
 ﻿function navigateWithWarning(event) {
     console.log(event);
     var url = event.target.href;
-    var message = "Are you sure you want to navigate to previous steps? You will have to pass Credit Check step again."
+    var message = "If you change Home Owner Information you will have to pass Credit Check step again"
     $('#alertModal').find('.modal-body p').html(message);
-    $('#alertModal').find('.modal-title').html('<svg aria-hidden="true" class="icon icon-reject"><use xlink:href="/client/Content/images/sprite/sprite.svg#icon-reject"></use></svg> Warning');
+    $('#alertModal').find('.modal-title').html('Navigate to step 1?');
+    $('#alertModal').find('#confirmAlert').html('Go to Step 1');
     $('#alertModal').modal('show');
     $('#confirmAlert').on('click', function(){
         window.location.href = url;
