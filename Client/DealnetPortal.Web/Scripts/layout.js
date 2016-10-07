2f58fb3826cd7a1ff8118e64bd47c82ddf61ee3e
oblonskaya <Oksana.Blonskaya@dataart.com>
2016-10-07 10:56:32 +0300

    dev-DEAL-327: Modal dialog windows layout (fixed styled select for dataTable )[0.0.7]

--- Client/DealnetPortal.Web/Scripts/layout.js
+++ Client/DealnetPortal.Web/Scripts/layout.js
@@ -143,12 +143,10 @@ function customizeSelect(){
   setTimeout(function(){
     $('select').each(function(){
       var selectClasses = $(this).hasClass("dealnet-disabled-input") ? "custom-select-disabled" : "custom-select";
-      if(!$(this).parents(".ui-datepicker").length){
-        if(!$(this).parents(".custom-select").length || !$(this).parents(".custom-select-disabled").length){
-          $(this).wrap('<div class='+selectClasses+'>');
-          if(detectIE() === false){
-            $(this).after('<span class="caret">');
-          }
+      if(!$(this).parents(".ui-datepicker").length && !$(this).parents(".custom-select").length && !$(this).parents(".custom-select-disabled").length){
+        $(this).wrap('<div class='+selectClasses+'>');
+        if(detectIE() === false){
+          $(this).after('<span class="caret">');
         }
       }
     });
