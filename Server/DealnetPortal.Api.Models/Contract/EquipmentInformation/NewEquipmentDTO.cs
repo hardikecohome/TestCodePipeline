706786eced31065c73cdac44b6ffc4298aea4b4f
Denis Tish <dtish@dataart.com>
2016-10-21 11:05:23 +0300

    Cost/quantity fields - intermediate

--- Server/DealnetPortal.Api.Models/Contract/EquipmentInformation/NewEquipmentDTO.cs
+++ Server/DealnetPortal.Api.Models/Contract/EquipmentInformation/NewEquipmentDTO.cs
@@ -9,12 +9,11 @@ namespace DealnetPortal.Api.Models.Contract.EquipmentInformation
     public class NewEquipmentDTO
     {
         public int Id { get; set; }
-        public int Quantity { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
 
 
-        public decimal Cost { get; set; }
+        public decimal? Cost { get; set; }
 
         public decimal MonthlyCost { get; set; }
 
