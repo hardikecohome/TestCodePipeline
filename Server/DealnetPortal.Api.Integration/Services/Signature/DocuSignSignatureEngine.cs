6616c21c3ed63ca56a59fc72e5c841d31756544c
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2016-10-26 12:36:58 +0300

    DocuSign implementation

--- Server/DealnetPortal.Api.Integration/Services/Signature/DocuSignSignatureEngine.cs
+++ Server/DealnetPortal.Api.Integration/Services/Signature/DocuSignSignatureEngine.cs
@@ -30,8 +30,10 @@ namespace DealnetPortal.Api.Integration.Services.Signature
         private Document _document { get; set; }
 
         private List<Text> _textTabs { get; set; }
-
-        private List<Checkbox> _checkboxTabs { get; set; } 
+        private List<Checkbox> _checkboxTabs { get; set; }
+        private List<SignHere> _signHereTabs { get; set; }
+        private List<Signer> _signers { get; set; }
+        private List<CarbonCopy> _copyViewers { get; set; }
 
 
         public DocuSignSignatureEngine()
@@ -106,11 +108,15 @@ namespace DealnetPortal.Api.Integration.Services.Signature
 
                 if (contract != null & agreementTemplate != null)
                 {
-                    _document = new Document();
-                    _document.DocumentBase64 = System.Convert.ToBase64String(agreementTemplate.AgreementForm);
-                    _document.Name = agreementTemplate.TemplateName;
-                    _document.DocumentId = contract.Id.ToString();
-                    _document.TransformPdfFields = "true";
+                    _document = new Document
+                    {
+                        DocumentBase64 = System.Convert.ToBase64String(agreementTemplate.AgreementForm),
+                        Name = agreementTemplate.TemplateName,
+                        DocumentId = contract.Id.ToString(),
+                        TransformPdfFields = "true"
+                    };
+                    _signers = new List<Signer>();
+                    _copyViewers = new List<CarbonCopy>();
                 }
             });                      
 
@@ -125,34 +131,56 @@ namespace DealnetPortal.Api.Integration.Services.Signature
 
             await Task.Run(() =>
             {
-                formFields.ForEach(ff =>
+                if (formFields?.Any() ?? false)
                 {
-                    if (ff.FieldType == FieldType.CheckBox)
+                    formFields.ForEach(ff =>
                     {
-                        _checkboxTabs.Add(new Checkbox()
+                        if (ff.FieldType == FieldType.CheckBox)
                         {
-                            TabLabel = ff.Name,
-                            Selected = ff.Value
-                        });
-                    }
-                    else //Text
-                    {
-                        _textTabs.Add(new Text()
+                            _checkboxTabs.Add(new Checkbox()
+                            {
+                                TabLabel = ff.Name,
+                                Selected = ff.Value
+                            });
+                        }
+                        else //Text
                         {
-                            TabLabel = ff.Name,
-                            Value = ff.Value
-                        });
-                    }
-                });
+                            _textTabs.Add(new Text()
+                            {
+                                TabLabel = ff.Name,
+                                Value = ff.Value
+                            });
+                        }
+                    });
+                }
             });
 
             return alerts;
         }
 
-        public Task<IList<Alert>> InsertSignatures(IList<SignatureUser> signatureUsers)
+        public async Task<IList<Alert>> InsertSignatures(IList<SignatureUser> signatureUsers)
         {
-            throw new NotImplementedException();
-        }
+            var alerts = new List<Alert>();
+            _signHereTabs = new List<SignHere>();
+
+            await Task.Run(() =>
+            {
+                if (signatureUsers?.Any() ?? false)
+                {
+                    int signN = 1;
+                    signatureUsers.Where(s => s.Role == SignatureRole.Signer).ForEach(s =>
+                    {
+                        _signers.Add(CreateSigner(s, signN++));
+                    });
+                    signatureUsers.Where(s => s.Role == SignatureRole.AdditionalApplicant).ForEach(s =>
+                    {
+                        _signers.Add(CreateSigner(s, signN++));
+                    });
+                }
+            });
+
+            return alerts;
+        }        
 
         public Task<IList<Alert>> SendInvitations(IList<SignatureUser> signatureUsers)
         {
@@ -170,6 +198,48 @@ namespace DealnetPortal.Api.Integration.Services.Signature
 
             string authHeader = Newtonsoft.Json.JsonConvert.SerializeObject(dsCreds);
             return authHeader;
+        }
+
+        private Signer CreateSigner(SignatureUser signatureUser, int routingOrder)
+        {
+            var signer = new Signer()
+            {
+                Email = signatureUser.EmailAddress,
+                Name = $"{signatureUser.FirstName} {signatureUser.LastName}",
+                RecipientId = routingOrder.ToString(),
+                RoutingOrder = routingOrder.ToString(),
+                Tabs = new Tabs()
+                {
+                    SignHereTabs = new List<SignHere>()
+                    {
+                        new SignHere()
+                        {
+                            TabLabel = $"Signature{routingOrder}"
+                        }
+                    }
+                }
+            };
+            if (routingOrder == 1)
+            {
+                if (_textTabs?.Any() ?? false)
+                {
+                    if (signer.Tabs.TextTabs == null)
+                    {
+                        signer.Tabs.TextTabs = new List<Text>();
+                    }
+                    signer.Tabs.TextTabs.AddRange(_textTabs);
+                }
+                if (_checkboxTabs?.Any() ?? false)
+                {
+                    if (signer.Tabs.CheckboxTabs == null)
+                    {
+                        signer.Tabs.CheckboxTabs = new List<Checkbox>();
+                    }
+                    signer.Tabs.CheckboxTabs.AddRange(_checkboxTabs);
+                }
+            }
+
+            return signer;
         }
     }
 
