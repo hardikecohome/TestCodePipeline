971532ea64830440369bd84d4eae3f1472e7d6c8
Denis Tish <dtish@dataart.com>
2016-10-27 14:49:24 +0300

    Changed SubDealerName to DisplayName to make it more clear

--- Server/DealnetPortal.Api.Models/Contract/ApplicationUserDTO.cs
+++ Server/DealnetPortal.Api.Models/Contract/ApplicationUserDTO.cs
@@ -5,7 +5,7 @@ namespace DealnetPortal.Api.Models.Contract
     public class ApplicationUserDTO
     {
         public string Id { get; set; }
-        public string SubDealerName { get; set; }
+        public string DisplayName { get; set; }
         public string UserName { get; set; }
         public IList<ApplicationUserDTO> SubDealers { get; set; }
     }
