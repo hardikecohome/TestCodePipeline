daf76a2612fc978353737ef43504d8a6c4308d72
Denis Tish <dtish@dataart.com>
2016-11-03 03:27:46 +0200

    No Validation for Additional Applicant's phone number

--- Client/DealnetPortal.Web/Scripts/contact-and-payment.js
+++ Client/DealnetPortal.Web/Scripts/contact-and-payment.js
@@ -1,18 +1,28 @@
 ﻿$(document).ready(function () {
-    $("#home-phone").rules("add", "required");
-    $("#cell-phone").rules("add", "required");
+    $(".home-phone").each(function () {
+        $(this).rules("add", "required");
+    });
+    $(".cell-phone").each(function () {
+        $(this).rules("add", "required");
+    });
     $("#bank-number").rules("add", "required");
     $("#transit-number").rules("add", "required");
     $("#account-number").rules("add", "required");
     $("#enbridge-gas-distribution-account").rules("add", "required");
     $("#meter-number").rules("add", "required");
     //
-    setValidationRelation($("#home-phone"), $("#cell-phone"));
-    setValidationRelation($("#cell-phone"), $("#home-phone"));
+    $('.mandatory-phones').each(function() {
+        var homePhone = $(this).find('.home-phone');
+        var cellPhone = $(this).find('.cell-phone');
+        if (homePhone.length && cellPhone.length) {
+            setValidationRelation(homePhone, cellPhone);
+            setValidationRelation(cellPhone, homePhone);
+        }
+    });
     setValidationRelation($("#enbridge-gas-distribution-account"), $("#meter-number"));
     setValidationRelation($("#meter-number"), $("#enbridge-gas-distribution-account"));
-    $("#home-phone").change();
-    $("#cell-phone").change();
+    $(".home-phone").change();
+    $(".cell-phone").change();
     $("#enbridge-gas-distribution-account").change();
     $("#meter-number").change();
     var initPaymentType = $("#payment-type").find(":selected").val();
