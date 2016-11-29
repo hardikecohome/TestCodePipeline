2473d9bde9a49307e523871e35b6685713aedfe8
Denis Tish <dtish@dataart.com>
2016-11-29 19:41:04 +0200

    hotfix

--- Client/DealnetPortal.Web/Models/NewRentalViewModels.cs
+++ Client/DealnetPortal.Web/Models/NewRentalViewModels.cs
@@ -75,6 +75,7 @@ namespace DealnetPortal.Web.Models
         [StringLength(10, MinimumLength = 3)]
         [RegularExpression(@"^[a-zA-Z0-9]+\s?[a-zA-Z0-9]+$", ErrorMessage = "Postal Code is in incorrect format")]
         public string PostalCode { get; set; }
+        [Required]
         [Display(Name = "Residence")]
         public ResidenceType ResidenceType { get; set; }
     }
