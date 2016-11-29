7edfcfa19cde3d3e5c36568f93d09f1185a965f9
oblonskaya <Oksana.Blonskaya@dataart.com>
2016-11-29 19:13:12 +0200

    DEAL-679: Change markup for layout and order of document upload section [0.0.1]

--- Client/DealnetPortal.Web/Scripts/layout.js
+++ Client/DealnetPortal.Web/Scripts/layout.js
@@ -170,6 +170,29 @@
         }
         //resizeEvent();
       });
+
+      /*Responsive tabs*/
+      if($('.responsive-tabs').length){
+        var uploadErrorIcon = '<span class="error-icon"><svg aria-hidden="true" class="icon icon-error-upload"><use xlink:href="'+urlContent+'Content/images/sprite/sprite.svg#icon-error-upload"></use></svg></span>';
+        var uploadSuccess = '<span class="custom-checkbox"><span class="checkbox-icon"><svg aria-hidden="true" class="icon icon-checked"><use xlink:href="'+urlContent+'Content/images/sprite/sprite.svg#icon-checked"></use></svg></span></span>';
+        var accordionArrows = '<span class="pill-arrow"><i class="glyphicon"></i></span>';
+
+        var uploadedStatus = $('<span>', {
+          class: 'upload-doc-status',
+          html: uploadErrorIcon + uploadSuccess});
+        uploadedStatus.prependTo('a[data-toggle="tab"]');
+        $(accordionArrows).appendTo('a[data-toggle="tab"]');
+
+        $('.responsive-tabs a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+          var currentTab = $(this);
+          var currentTabParent = currentTab.parents('.documents-pills-item');
+          var currentTabId = currentTab.attr('aria-controls');
+
+          $('a[data-toggle="tab"]').parents('.documents-pills-item').removeClass('active');
+          $('a[aria-controls="'+ currentTabId +'"]').parents('.documents-pills-item').addClass('active');
+          currentTab.parents('.documents-pills-item').addClass('active');
+        });
+      }
 });
 
 function stickySection(elem){
