525d1588268749aa6ce34de99e9d5607c85b339b
Denis Tish <dtish@dataart.com>
2016-12-13 17:09:48 +0200

    hotfix

--- Client/DealnetPortal.Web/Models/NewRentalViewModels.cs
+++ Client/DealnetPortal.Web/Models/NewRentalViewModels.cs
@@ -188,10 +188,10 @@ namespace DealnetPortal.Web.Models
     {
         public override bool IsValid(object value)
         {
-            DateTime date = (DateTime)value;
+            var date = (DateTime)value;
             var todayDate = DateTime.Today;
             //In case of 29 February we make it 1 March (because date with 29 February minus 18 years equals date with 28 February)
-            return date > (todayDate.Day == 29 ? todayDate.AddDays(1).AddYears(-18) : todayDate);
+            return date <= (todayDate.Day == 29 ? todayDate.AddDays(1).AddYears(-18) : todayDate.AddYears(-18));
         }
     }
 }
