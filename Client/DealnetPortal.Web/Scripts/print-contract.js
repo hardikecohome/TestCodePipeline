47c230850830f82fc42d74b93b7c993a40e6e9be
Denis Tish <dtish@dataart.com>
2017-01-06 14:57:19 +0200

    Add "Print contract" button to Contract Update page

---
+++ Client/DealnetPortal.Web/Scripts/print-contract.js
@@ -0,0 +1,17 @@
+﻿function checkPrintedContract(checkUrl, downloadUrl) {
+    $.ajax({
+        type: "POST",
+        url: checkUrl,
+        success: function (json) {
+            if (json == true) {
+                $('#print-error-message').hide();
+                window.location = downloadUrl;
+            } else {
+                $('#print-error-message').show();
+            }
+        },
+        error: function (xhr, status, p3) {
+            $('#print-error-message').show();
+        }
+    });
+}
