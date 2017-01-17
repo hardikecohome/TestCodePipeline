359a87d0527ed6df6ebcd8a6d0e4291e4b2307a2
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2017-01-17 18:24:19 +0300

    refactoring of Aspire queries

---
+++ Server/DealnetPortal.DataAccess/IQueriesStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealnetPortal.DataAccess
+{
+    /// <summary>
+    /// Get sql query by name
+    /// </summary>
+    public interface IQueriesStorage
+    {
+        Dictionary<string, string> Queries { get; }
+        string GetQuery(string queryName);
+    }
+}
