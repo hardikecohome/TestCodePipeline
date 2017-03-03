b5c65bded88e46e0ad6a289f342d60f75289ee14
Denis Tish <dtish@dataart.com>
2017-03-03 17:42:57 +0200

    DEAL-1200 Error occurs and user cannot proceed through the flow

--- Client/DealnetPortal.Web/Models/NewRentalViewModels.cs
+++ Client/DealnetPortal.Web/Models/NewRentalViewModels.cs
@@ -199,6 +199,7 @@ namespace DealnetPortal.Web.Models
     {
         public override bool IsValid(object value)
         {
+            if (!(value is DateTime)) { return false; }
             var date = (DateTime)value;
             var todayDate = DateTime.Today;
             //In case of 29 February we make it 1 March (because date with 29 February minus 18 years equals date with 28 February)
