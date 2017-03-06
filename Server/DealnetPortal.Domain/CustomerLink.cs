f0eeab2c4823ae8296c301931922dde56f3699d5
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2017-03-06 18:11:21 +0300

    new models for customer link

---
+++ Server/DealnetPortal.Domain/CustomerLink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealnetPortal.Domain
+{
+    public class CustomerLink
+    {
+        public CustomerLink()
+        {
+            Services = new HashSet<DealerService>();
+        }
+        public int Id { get; set; }
+        public bool IsEnglishEnabled { get; set; }
+        public bool IsFrenchEnabled { get; set; }
+
+        public virtual ICollection<DealerService> Services { get; set; }
+
+        public Customer Customer { get; set; }
+    }
+}
