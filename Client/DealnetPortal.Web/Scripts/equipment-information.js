61030abbe00c7421b63af35e3ca6655e409bc6b0
Gonzalo Rafael Acosta <gonzalo.acosta@dataart.com>
2017-03-20 16:10:36 -0300

    Run calculateTotalCashPrice after configuration is inited in summaryAndConfirmation

--- Client/DealnetPortal.Web/Scripts/equipment-information.js
+++ Client/DealnetPortal.Web/Scripts/equipment-information.js
@@ -265,7 +265,7 @@ function recalculateTotalCashPrice() {
     });
 
     if (!sum) { return; }
-    $("#equipment-cash-price").text(sum.toFixed(2));
+    $("#equipment-cash-price").text(numberFormatter(sum));
     calculateLoanValues();
 }
 
