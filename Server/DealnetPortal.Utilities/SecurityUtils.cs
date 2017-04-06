132426c6942f7cc65ec60e0c04baaecfa0d61ceb
Dzmitry Rusak <rusak.dmitry@gmail.com>
2017-04-06 15:06:18 +0200

    DEAL-1428: 404 error because of email login

--- Server/DealnetPortal.Utilities/SecurityUtils.cs
+++ Server/DealnetPortal.Utilities/SecurityUtils.cs
@@ -27,5 +27,22 @@ namespace DealnetPortal.Utilities
             Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
             return Convert.ToBase64String(dst);
         }
+
+        public static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
     }
 }
