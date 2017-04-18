2d1bd2c20f0e093c25a100b584d6e2e8d1aedebd
Denis Tish <dtish@dataart.com>
2017-04-18 18:16:06 +0300

    Initial actions and views

---
+++ Client/DealnetPortal.Web/Controllers/MortgageBrokerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DealnetPortal.Web.Models;
+
+namespace DealnetPortal.Web.Controllers
+{
+
+    public class MortgageBrokerController : Controller
+    {
+        public ActionResult NewCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NewCustomer(NewCustomerViewModel newCustomer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return RedirectToAction("CustomerCreationSuccess", new { contractId =  0 });
+        }
+
+        public ActionResult CustomerCreationSuccess(int contractId)
+        {
+            return View();
+        }
+
+        public ActionResult MyCustomers()
+        {
+            return View();
+        }
+    }
+}
