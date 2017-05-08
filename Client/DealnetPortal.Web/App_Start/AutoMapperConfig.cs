2ae2c5fb0f48376a1f3b522093a4827f856a2396
Dzmitry Rusak <rusak.dmitry@gmail.com>
2017-05-08 17:05:05 +0200

    DEAL-1191: Allow dealer to select financing program on step 2 based on his rate card

--- Client/DealnetPortal.Web/App_Start/AutoMapperConfig.cs
+++ Client/DealnetPortal.Web/App_Start/AutoMapperConfig.cs
@@ -184,7 +184,7 @@ namespace DealnetPortal.Web.App_Start
                 .ForMember(x => x.InstallationDate, d => d.Ignore())
                 .ForMember(x => x.InstallerFirstName, d => d.Ignore())
                 .ForMember(x => x.InstallerLastName, d => d.Ignore())
-                .ForMember(x => x.RateCardId, s=>s.MapFrom( d=>d.SelectedRateCardId))
+                //.ForMember(x => x.RateCardId, s=>s.MapFrom( d=>d.SelectedRateCardId))
                 .ForMember(x => x.DeferralType, d => d.ResolveUsing(src => src.AgreementType == AgreementType.LoanApplication ? src.LoanDeferralType.ConvertTo<DeferralType>() : src.RentalDeferralType.ConvertTo<DeferralType>()));
         }
 
@@ -335,7 +335,7 @@ namespace DealnetPortal.Web.App_Start
             //New Version
             cfg.CreateMap<EquipmentInfoDTO, EquipmentInformationViewModelNew>()
                 .ForMember(x => x.ContractId, d => d.MapFrom(src => src.Id))
-                .ForMember(x => x.SelectedRateCardId, d => d.MapFrom(o => o.RateCardId))
+                //.ForMember(x => x.SelectedRateCardId, d => d.MapFrom(o => o.RateCardId))
                 .ForMember(x => x.ProvinceTaxRate, d => d.Ignore())
                 .ForMember(x => x.CreditAmount, d => d.Ignore())
                 .ForMember(x => x.LoanDeferralType, d => d.ResolveUsing(src => src.AgreementType == Api.Common.Enumeration.AgreementType.LoanApplication ? src.DeferralType : 0))
