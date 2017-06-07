fe5b35354c64f10b08adecfd88c68f8f7bb67a2f
Dmitry Petlya <dmitry.petlya@dataart.com>
2017-06-07 15:29:25 +0300

    DEAL-1956, DEAL-1958, DEAL-1957, DEAL-1953

--- Client/DealnetPortal.Web/App_Start/BundleConfig.cs
+++ Client/DealnetPortal.Web/App_Start/BundleConfig.cs
@@ -42,7 +42,8 @@ namespace DealnetPortal.Web
                      "~/Scripts/datatables/dataTables.responsive.js"));
             bundles.Add(new ScriptBundle("~/bundles/home-page").Include("~/Scripts/jquery.form.js").Include(
                 "~/Scripts/chart.js", "~/Scripts/home-page.js"));
-            bundles.Add(new ScriptBundle("~/bundles/basic-info").Include("~/Scripts/general-address-autocomplete.js")
+            bundles.Add(new ScriptBundle("~/bundles/basic-info")
+                .Include("~/Scripts/general-address-autocomplete.js")
                 .Include("~/Scripts/customer-validation.js")
                 .IncludeDirectory("~/Scripts/BasicInfo", "*.js").Include(
                 "~/Scripts/BasicInfo/DlScanning/dl-scanning.js").Include("~/Scripts/datejs.js")
@@ -60,12 +61,19 @@ namespace DealnetPortal.Web
             bundles.Add(new ScriptBundle("~/bundles/contact-and-payment").Include("~/Scripts/custom-validation.js")
                 .Include("~/Scripts/contact-and-payment-management.js")/*.Include("~/Scripts/camera-capturing.js")*/
                                                                        /*.Include("~/Scripts/vc-scanning.js")*/.Include("~/Scripts/navigation-with-warning.js"));
-            bundles.Add(new ScriptBundle("~/bundles/summary-and-confirmation").Include("~/Scripts/custom-validation.js")
-                .Include("~/Scripts/province-codes-helper.js").Include("~/Scripts/financial-functions.js")
-                .Include("~/Scripts/loan-calculator.js").Include("~/Scripts/contact-and-payment-management.js")
-                .Include("~/Scripts/summary-and-confirmation.js").Include("~/Scripts/jquery.form.js")
+
+            bundles.Add(new ScriptBundle("~/bundles/summary-and-confirmation")
                 .Include("~/Scripts/general-address-autocomplete.js")
-                .Include("~/Scripts/editable-in-modal.js").Include("~/Scripts/navigation-with-warning.js"));
+                .Include("~/Scripts/custom-validation.js")
+                .Include("~/Scripts/province-codes-helper.js")
+                .Include("~/Scripts/financial-functions.js")
+                .Include("~/Scripts/loan-calculator.js")
+                .Include("~/Scripts/contact-and-payment-management.js")
+                .Include("~/Scripts/summary-and-confirmation.js")
+                .Include("~/Scripts/jquery.form.js")
+                .Include("~/Scripts/editable-in-modal.js")
+                .Include("~/Scripts/navigation-with-warning.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/agreement-submit-success").Include("~/Scripts/jquery.form.js")
                 .Include("~/Scripts/print-contract.js").Include("~/Scripts/agreement-submit-success.js"));
             bundles.Add(new ScriptBundle("~/bundles/my-customers").Include("~/Scripts/datejs.js").Include("~/Scripts/my-customers.js"));
