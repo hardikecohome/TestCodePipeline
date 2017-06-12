d6f5ffe8b367634968be83abbb0402c2c554b8e9
Dmitry Petlya <dmitry.petlya@dataart.com>
2017-06-12 17:35:18 +0300

    fix regarding DEAL-2026

--- Client/DealnetPortal.Web/Scripts/leads.js
+++ Client/DealnetPortal.Web/Scripts/leads.js
@@ -54,7 +54,8 @@ function showTable() {
                             "sPrevious": '<i class="glyphicon glyphicon-menu-left"></i>'
                         },
                         "sLengthMenu": translations['Show'] + " _MENU_ " + translations['Entries'],
-                        "sZeroRecords": translations['NoMatchingRecordsFound']
+                        "sZeroRecords": translations['NoMatchingRecordsFound'],
+                        "sEmptyTable": translations['LeadsNoMatchingRecordsFound']
                     },
                     columns: [
 					    { "data": "Date", className: 'date-cell expanded-cell' },
