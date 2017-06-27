5fd0f004a1190fb516e954f085324a3bb7ca1b52
Dzmitry Rusak <dmitry.rusak@dataart.com>
2017-06-27 15:14:51 +0200

    DEAL-2136: Add checking web config keys before server running

--- Server/DealnetPortal.Api/Global.asax.cs
+++ Server/DealnetPortal.Api/Global.asax.cs
@@ -30,6 +30,7 @@ namespace DealnetPortal.Api
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             DatabaseConfig.Initialize();
             AutoMapperConfig.Configure();
+            WebApiConfig.CheckConfigKeys();
 
             //GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerSelector),
             //    new LocalizedControllerSelector(GlobalConfiguration.Configuration));
