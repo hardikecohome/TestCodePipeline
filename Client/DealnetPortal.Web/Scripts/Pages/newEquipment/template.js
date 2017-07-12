48fbdce45937519656d751eeab3e7a4868e27195
Dmitry Petlya <dmitry.petlya@dataart.com>
2017-07-12 15:40:02 +0300

    DEAL-2516 fix with Note field

--- Client/DealnetPortal.Web/Scripts/Pages/newEquipment/template.js
+++ Client/DealnetPortal.Web/Scripts/Pages/newEquipment/template.js
@@ -1,12 +1,7 @@
 ﻿module.exports('equipment-template', function() {
     var equipmentFactory = function (parentNode, options) {
         var template = document.getElementById(options.templateId).innerHTML;
-        //var equipmentItemsLength = $(options.equipmentDiv).find('[id^=new-equipment-]').length;
         var equipmentItemsLength = $(options.equipmentDiv).find('[id^=' + options.equipmentIdPattern +']').length;
-        //var result = template.split(options.equipmentName + '[0]'"NewEquipment[0]")
-        //    .join("NewEquipment[" + options.id + "]")
-        //    .split("NewEquipment_0").join("NewEquipment_" + options.id)
-        //    .replace("№1", "№" + (equipmentItemsLength + 1));
         
         var result = template.split(options.equipmentName + '[0]')
         .join(options.equipmentName + '[' + options.id + ']')
@@ -14,10 +9,13 @@
             .replace("№1", "№" + (equipmentItemsLength + 1));
 
         parentNode.append($.parseHTML(result));
-        //$(parentNode).attr('id', 'new-equipment-' + options.id);
         $(parentNode).attr('id', options.equipmentIdPattern + options.id);
 
-        
+        // Html.TextAreaFor doesn't contain default value parameter
+        if (options.equipmentName === 'ExistingEquipment') {
+            $(parentNode).find('#ExistingEquipment_' + options.id + '__Notes').text('');
+        }
+
         if (options.id === 0 && options.equipmentName === 'NewEquipment') {
             $(parentNode).find("div.additional-remove").remove();
         }
