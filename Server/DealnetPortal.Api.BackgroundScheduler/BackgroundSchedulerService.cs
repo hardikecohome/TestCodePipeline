e7522cdbecc9a519212ed60e6a8d326cbb95af19
Dzmitry Rusak <dmitry.rusak@dataart.com>
2017-07-24 13:28:03 +0200

    More logging information added

--- Server/DealnetPortal.Api.BackgroundScheduler/BackgroundSchedulerService.cs
+++ Server/DealnetPortal.Api.BackgroundScheduler/BackgroundSchedulerService.cs
@@ -27,13 +27,21 @@ namespace DealnetPortal.Api.BackgroundScheduler
             try
             {
                 var contracts = _contractRepository.GetExpiredContracts(expiredDateTime);
+                _loggingService.LogInfo($"There are {contracts.Count} expired contracts.");
                 foreach (var contract in contracts)
                 {
                     _loggingService.LogInfo($"Sending infromation for contract id = {contract.Id}...");
-                    _mailService.SendNotifyMailNoDealerAcceptedLead12H(contract);
+                    try
+                    {
+                        _mailService.SendNotifyMailNoDealerAcceptedLead12H(contract);
+                    }
+                    catch (Exception ex)
+                    {
+                        _loggingService.LogInfo($"Sending contract id = {contract.Id} trow exeption with message: {ex.Message}");
+                    }
                     _loggingService.LogInfo($"Infromation for contract id = {contract.Id} has sent.");
                 }
-                _loggingService.LogInfo($"Checking expired leads finished at {DateTime.Now}. There are {contracts.Count} expired contracts");
+                _loggingService.LogInfo($"Checking expired leads finished at {DateTime.Now}.");
             }
             catch (Exception ex)
             {
