c19faf240ed5add3bf5bdd093dd019d87e7d0065
hardikecohome <HSharma@ecohomefinancial.com>
2017-08-04 11:26:23 -0400

    Missed commentting sms for dealer accepted lead.

--- Server/DealnetPortal.Api.Integration/Services/MailService.cs
+++ Server/DealnetPortal.Api.Integration/Services/MailService.cs
@@ -605,10 +605,9 @@ namespace DealnetPortal.Api.Integration.Services
                 //await _emailService.SendAsync(mail);
                
                 //await _mailChimpService.AddNewSubscriberAsync(ConfigurationManager.AppSettings["ListID"], member);
-                var result = await _personalizedMessageService.SendMessage(contract.PrimaryCustomer.Phones.FirstOrDefault(p => p.PhoneType == PhoneType.Cell).PhoneNum, subject);
+                //var result = await _personalizedMessageService.SendMessage(contract.PrimaryCustomer.Phones.FirstOrDefault(p => p.PhoneType == PhoneType.Cell).PhoneNum, subject);
                 if (await _mailChimpService.isSubscriber(ConfigurationManager.AppSettings["ListID"], contract.PrimaryCustomer.Emails.FirstOrDefault().EmailAddress))
-                {
-                    
+                {                    
                     await _mandrillService.SendDealerLeadAccepted(contract, dealer, services);
                 }
             }
