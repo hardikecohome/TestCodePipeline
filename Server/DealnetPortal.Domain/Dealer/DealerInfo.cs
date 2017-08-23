c452053093b7a4cae48e2e7de73e284a2dc2f0e2
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2017-08-23 19:03:14 +0300

    started entities creation

---
+++ Server/DealnetPortal.Domain/Dealer/DealerInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealnetPortal.Domain.Dealer
+{
+    public class DealerInfo
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Link to a parent dealer
+        /// </summary>
+        public string ParentSalesRepId { get; set; }
+        [ForeignKey(nameof(ParentSalesRepId))]
+        public ApplicationUser ParentSalesRep { get; set; }
+        /// <summary>
+        /// Link to a draft for continue editing
+        /// </summary>
+        [Index("LinkIndex", IsUnique = true)]
+        public string DraftLink { get; set; }
+    }
+}
