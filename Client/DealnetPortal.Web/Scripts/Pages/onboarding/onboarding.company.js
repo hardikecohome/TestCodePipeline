51184c5d3c737c94b7bab65628aeab519c915c0c
Maccabee0 <marcus.brinda@dataart.com>
2017-08-25 12:40:32 +0300

    Add Province tag

--- Client/DealnetPortal.Web/Scripts/Pages/onboarding/onboarding.company.js
+++ Client/DealnetPortal.Web/Scripts/Pages/onboarding/onboarding.company.js
@@ -1,9 +1,16 @@
-﻿module.exports('onboarding.company',function (require) {
+﻿module.exports('onboarding.company', function (require) {
     var state = require('onboarding.state').state;
 
-    var provinceTemplate = function (province) {
-        var template = $('#province-template').innerHTML();
-        debugger
+    var provinceTemplate = function (id, province) {
+        var template = $('#province-template').html();
+        var result = template.split('province-0')
+            .join('province-' + id)
+            .split('Province[0]')
+            .join('Province[' + id + ']');
+        var $result = $(result);
+        $result.find('#province-' + id + '-display').text(province);
+        $result.find('#province-' + id).val(province);
+        return $result;
     };
 
     var init = function () {
@@ -11,16 +18,36 @@
             var $this = $(this);
             var id = $this.attr('id').split('-')[1];
             state.selectedProvinces.push({ id: id, province: $this.val() });
+            setRemoveClick(id);
             state.nextProvinceId++;
-            $('#province-' + id + '-remove').on('click', remove);
         });
+    };
+
+    var add = function () {
+        var value = this.value;
+        if (value) {
+            if (state.selectedProvinces.indexOf(value) === -1) {
+                state.selectedProvinces.push({ id: state.nextProvinceId, province: value });
+
+                $('#province-list').append(provinceTemplate(state.nextProvinceId, value));
+                setRemoveClick(state.nextProvinceId);
+
+                state.nextProvinceId++;
+            }
+            $(this).val('');
+        }
     };
 
     var remove = function () {
 
     };
 
+    var setRemoveClick = function (id) {
+        $('#province-' + id + '-remove').on('click', remove);
+    };
+
     return {
-        initCompany: init
+        initCompany: init,
+        addProvince: add
     };
 });
--- Client/DealnetPortal.Web/Scripts/Pages/onboarding/onboarding.index.js
+++ Client/DealnetPortal.Web/Scripts/Pages/onboarding/onboarding.index.js
@@ -1,9 +1,11 @@
 ﻿module.exports('onboarding.index', function (require) {
     var initCompany = require('onboarding.company').initCompany;
+    var addProvince = require('onboarding.company').addProvince;
 
 
     function init() {
         initCompany();
+        $('#province-select').on('change', addProvince);
     }
 
     return {
