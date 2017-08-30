1d6dc53c0e3bbc9cdc47fd450f027f56f063cdbc
Dmitry Petlya <dmitry.petlya@dataart.com>
2017-08-30 16:11:30 +0300

    add client cosent scripts

--- Client/DealnetPortal.Web/Scripts/Pages/onboarding/onboarding.index.js
+++ Client/DealnetPortal.Web/Scripts/Pages/onboarding/onboarding.index.js
@@ -2,12 +2,15 @@
     var company = require('onboarding.company');
     var product = require('onboarding.product');
     var ownerInfo = require('onboarding.owner-info.index');
+    var consent = require('onboarding.consent.index');
     var validateAndSubmit = require('onboarding.form.handlers');
 
     function init() {
         company.initCompany();
         product.initProducts();
         ownerInfo.init();
+        consent.init();
+
         $('#submit').on('click', validateAndSubmit);
     }
 
