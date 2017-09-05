790a02046a5afbd8646247af5da3337389b51851
Dzmitry Rusak <dmitry.rusak@dataart.com>
2017-09-05 15:25:09 +0200

    DEAL-2859: Characters entered by user in Postal Code field should be uppercase

--- Client/DealnetPortal.Web/Scripts/Pages/onboarding/company/onboarding.company.index.js
+++ Client/DealnetPortal.Web/Scripts/Pages/onboarding/company/onboarding.company.index.js
@@ -4,12 +4,13 @@
     var setRemoveClick = require('onboarding.company.province').setRemoveClick;
     var setters = require('onboarding.company.setters');
 
-    var init = function () {
-        $('input[id^="province-"]').each(function () {
-            var $this = $(this);
-            var id = $this.attr('id').split('-')[1];
-            setRemoveClick($this.val());
-        });
+    var init = function() {
+        $('input[id^="province-"]')
+            .each(function() {
+                var $this = $(this);
+                var id = $this.attr('id').split('-')[1];
+                setRemoveClick($this.val());
+            });
         $('#province-select').on('change', add);
 
         $('#full-legal-name').on('change', setters.setLegalName);
@@ -21,7 +22,10 @@
         $('#company-unit').on('change', setters.setUnit);
         $('#company-city').on('change', setters.setCity);
         $('#company-province').on('change', setters.setProvince);
-        $('#company-postal').on('change', setters.setPostalCode);
+        $('#company-postal').on('change',function (e) {
+                    e.target.value = e.target.value.toUpperCase();
+                    setters.setPostalCode();
+            });
         $('#company-type').on('change', setters.setType);
         $('#years-in-business').on('change', setters.setYearsInBusiness);
         $('#installers').on('change', setters.setInstallers);
