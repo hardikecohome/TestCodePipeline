24f6f7e4b57fed11ab528d5452495c77730ab244
Dmitry Petlya <dmitry.petlya@dataart.com>
2017-09-11 13:24:51 +0300

    DEAL-2878

--- Client/DealnetPortal.Web.Resources/Resources.Designer.cs
+++ Client/DealnetPortal.Web.Resources/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace DealnetPortal.Web.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -4831,7 +4831,7 @@ namespace DealnetPortal.Web.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Something went wrong.
+        ///   Looks up a localized string similar to Something went wrong..
         /// </summary>
         public static string SomethingWentWrong {
             get {
