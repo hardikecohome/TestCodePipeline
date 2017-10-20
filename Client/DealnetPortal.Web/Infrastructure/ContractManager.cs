ddf04c657ef14d6375aa80fd7bce115d0d50ae55
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2017-10-20 19:01:04 +0300

    dev-DEAL-3128

--- Client/DealnetPortal.Web/Infrastructure/ContractManager.cs
+++ Client/DealnetPortal.Web/Infrastructure/ContractManager.cs
@@ -205,7 +205,7 @@ namespace DealnetPortal.Web.Infrastructure
             }
             await MapSummary(summaryAndConfirmation, contractResult, contractId);
             var dealerTier = await _contractServiceAgent.GetDealerTier(contractId);
-            summaryAndConfirmation.RateCardValid = !contractResult.Equipment.RateCardId.HasValue || contractResult.Equipment.RateCardId.Value == 0 || dealerTier.RateCards.Any(x => x.Id == contractResult.Equipment.RateCardId.Value);
+            summaryAndConfirmation.RateCardValid = !(contractResult.Equipment?.RateCardId.HasValue ?? false) || contractResult.Equipment.RateCardId.Value == 0 || dealerTier.RateCards.Any(x => x.Id == contractResult.Equipment.RateCardId.Value);
             return summaryAndConfirmation;
         }
 
