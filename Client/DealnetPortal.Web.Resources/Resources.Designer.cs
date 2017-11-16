fe221579d21ab59b886145d05986c74cbe0c6b78
Dmitry Petlya <dmitry.petlya@dataart.com>
2017-11-16 15:44:21 +0200

    rename partial view, move button

--- Client/DealnetPortal.Web.Resources/Resources.Designer.cs
+++ Client/DealnetPortal.Web.Resources/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace DealnetPortal.Web.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -4526,6 +4526,15 @@ namespace DealnetPortal.Web.Resources {
         public static string PrincipalOwnerInformation {
             get {
                 return ResourceManager.GetString("PrincipalOwnerInformation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Print Signed Contract.
+        /// </summary>
+        public static string PrintSignedContract {
+            get {
+                return ResourceManager.GetString("PrintSignedContract", resourceCulture);
             }
         }
         
