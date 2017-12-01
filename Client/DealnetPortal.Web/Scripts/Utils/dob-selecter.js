7773659f744d40fe43676e47c63a61732ad468e8
Maccabee0 <marcus.brinda@dataart.com>
2017-12-01 11:52:15 +0200

    DEAL-3353

--- Client/DealnetPortal.Web/Scripts/Utils/dob-selecter.js
+++ Client/DealnetPortal.Web/Scripts/Utils/dob-selecter.js
@@ -85,7 +85,9 @@
         if (day && month && year) {
             $input.val(month + '/' + day + '/' + year).change();
         } else {
-            $input.val('').change();
+            if ($input.val() !== '') {
+                $input.val('').change();
+            }
         }
     }
 
@@ -101,14 +103,20 @@
 
     $.validator.addMethod("over18",
         function (value, element) {
-            debugger
             var minDate = new Date("1900-01-01");
             var maxDate = new Date(new Date().setFullYear(new Date().getFullYear() - 18));
             var valueEntered = new Date(value);
 
             if (valueEntered > maxDate) {
+                var selects = $(element).parents('.dob-group').find('select');
+                selects.blur();
+                // timeout added because of .blur not taking immediate effect and focused dropdown stays valid
+                setTimeout(function () {
+                    selects.removeClass('valid').addClass('input-validation-error');
+                }, 0);
                 return false;
             }
+            $(element).parents('.dob-group').find('select').addClass('valid').removeClass('input-validation-error');
             return true;
         },
         translations['Over18']
