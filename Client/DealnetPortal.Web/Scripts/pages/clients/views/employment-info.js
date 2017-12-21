adf0fd7b917aa7de38bee374d9b3c0b021fa70a0
Maccabee0 <marcus.brinda@dataart.com>
2017-12-21 17:49:08 +0200

    DEAL-3476 [0.0.2]

---
+++ Client/DealnetPortal.Web/Scripts/pages/clients/views/employment-info.js
@@ -0,0 +1,23 @@
+﻿module.exports('employment-info-view', function (require) {
+    var clientActions = require('new-client-actions');
+    var createAction = require('redux').createAction;
+    var readInitialStateFromFields = require('objectUtils').readInitialStateFromFields;
+
+    return function (store) {
+        var dispatch = store.dispatch;
+
+        var status = $('#employment-status');
+
+        var incomeType = $('#income-type');
+
+        var annual = $('#annual');
+
+        var hourly = $('#hourly');
+
+        var years = $('#emp-years');
+
+        var months = $('#emp-months');
+
+        var
+    }
+});
