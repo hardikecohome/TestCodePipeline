28a5d5c64506e53d6f7f1aff6c4747b4f010f14b
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2017-12-22 17:08:56 +0300

    DEAL-3772, DEAL-3761

--- Server/DealnetPortal.Aspire.Integration/Models/DocumentUploadRequest.cs
+++ Server/DealnetPortal.Aspire.Integration/Models/DocumentUploadRequest.cs
@@ -1,27 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
 using System.Xml.Serialization;
 
 namespace DealnetPortal.Aspire.Integration.Models
 {
     [Serializable]
     [XmlRoot(ElementName = "DocumentUploadXML")]
-    public class DocumentUploadRequest : DealUploadRequest
+    public class DocumentUploadRequest : DealUploadRequest, IXmlSerializable
     {
-        //public RequestHeader Header { get; set; }
+        private DocumentUploadPayload _payload;
+        public override Payload Payload
+        {
+            get { return _payload; }
+            set { _payload = value as DocumentUploadPayload; }
+        }
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
 
-        public new DocumentUploadPayload Payload { get; set; }
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            if (Header != null)
+            {
+                writer.WriteStartElement("Header");
+                writer.WriteElementString("UserId", Header.UserId);
+                writer.WriteElementString("Password", Header.Password);
+                writer.WriteEndElement();
+            }
+            if (Payload != null)
+            {
+                writer.WriteStartElement("Payload");
+                writer.WriteElementString("TransactionId", _payload.TransactionId);
+                writer.WriteElementString("Status", _payload.Status);
+                _payload.Documents?.ForEach(doc =>
+                {
+                    writer.WriteStartElement("Document");
+                    if (!string.IsNullOrEmpty(doc?.Name))
+                    {
+                        writer.WriteElementString("Name", doc.Name);
+                    }
+                    if (!string.IsNullOrEmpty(doc?.Data))
+                    {
+                        writer.WriteElementString("Data", doc.Data);
+                    }
+                    if (!string.IsNullOrEmpty(doc?.Ext))
+                    {
+                        writer.WriteElementString("Ext", doc.Ext);
+                    }
+                    writer.WriteEndElement();
+                });
+                writer.WriteEndElement();
+            }
+        }
     }
 
     [Serializable]
     public class DocumentUploadPayload : Payload
     {
         public string ContractId { get; set; }
-        //public string TransactionId { get; set; }
         public string Status { get; set; }
 
         [XmlElement("Document")]
-        public List<Document> Documents { get; set; }
+        public List<Document> Documents { get; set; }        
     }
 
     [Serializable]
