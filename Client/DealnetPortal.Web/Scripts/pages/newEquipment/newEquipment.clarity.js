90c16abc738c13fe0268389995a5e47a66f62ed0
Maccabee0 <marcus.brinda@dataart.com>
2018-01-12 12:56:43 +0200

    DEAL-3657 [0.0.3]

---
+++ Client/DealnetPortal.Web/Scripts/pages/newEquipment/newEquipment.clarity.js
@@ -0,0 +1,32 @@
+﻿module.exports('clarity', function() {
+    
+    var showLoanDetails = function() {
+        $('#loanRateCardToggle').find('i.glyphicon')
+            .removeClass('glyphicon-chevron-down')
+            .addClass('glyphicon-chevron-up');
+
+        $('.loan-details').removeClass('hidden');
+        $('.loan-brief').addClass('hidden');
+    };
+
+    var hideLoanDetails = function() {
+        $('#loanRateCardToggle').find('i.glyphicon')
+            .removeClass('glyphicon-chevron-up')
+            .addClass('glyphicon-chevron-down');
+
+        $('.loan-details').addClass('hidden');
+        $('.loan-brief').removeClass('hidden');
+    };
+
+    var init = function () {
+        var opened = true;
+        $('#loanRateCardToggle').on('click', function() {
+            opened ? hideLoanDetails() : showLoanDetails();
+            opened = !opened;
+        });
+    };
+
+    return {
+        init: init
+    };
+});
