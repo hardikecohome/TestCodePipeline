25a4c665cce7d3f6d9174d2f1955cf9dd0936d19
Maccabee0 <marcus.brinda@dataart.com>
2018-02-02 14:29:33 +0200

    My Deals count fixes

--- Client/DealnetPortal.Web/Scripts/Utils/myDeals.counter.js
+++ Client/DealnetPortal.Web/Scripts/Utils/myDeals.counter.js
@@ -5,12 +5,10 @@
             type: "GET",
             url: customerDealsCountUrl,
             success: function (json) {
-                var number = $('#new-deals-number');
                 if (json && json.dealsCount && json.dealsCount !== 0) {
-                    number.text(json.dealsCount + ' ' + translations['new']);
-                    number.show();
+                    $('.new-deals-number').text(json.dealsCount + ' ' + translations['new']);
                 } else {
-                    number.hide();
+                    $('.new-deals-number').parent().hide();
                 }
             }
         });
