9ccf23019acbc513ed5bebb3dad027877c405bda
Dzmitry Rusak <dmitry.rusak@dataart.com>
2018-02-16 13:55:49 +0100

    Test fix

--- Client/DealnetPortal.Web/Startup.cs
+++ Client/DealnetPortal.Web/Startup.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
+using Microsoft.AspNet.Identity;
 
 [assembly: OwinStartup(typeof(DealnetPortal.Web.Startup))]
 
@@ -49,12 +50,31 @@ namespace DealnetPortal.Web
                 context.RedirectUri = url.Action("Login", "Account", routeValues);
                 originalHandler.Invoke(context);
             };
+            provider.OnValidateIdentity = ctx =>
+            {
+                //MyClaimsIdentityObject si = MyApp.Identity.Current();
+                //if (si == null || si.UserId == 0 || si.CustomerId == 0)
+                {
+                    //ctx.RejectIdentity();
+                    // what needs to happen here for a return value?
+                }
+                return Task.FromResult<int>(0);
+            };
+            //provider.OnValidateIdentity = MySecurityStampValidator.OnValidateIdentity(
+            //    validateInterval: TimeSpan.FromMinutes(0),
+            //    regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager));
+
+            //provider.OnResponseSignIn = (context) =>
+            //{
+            //    context.Properties.IsPersistent = false;
+            //    context.Properties.ExpiresUtc = DateTimeOffset.Now.AddSeconds(10);
+            //};
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString(url.Action("Login", "Account")),
-                Provider = provider
+                Provider = provider,
             });
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
 
