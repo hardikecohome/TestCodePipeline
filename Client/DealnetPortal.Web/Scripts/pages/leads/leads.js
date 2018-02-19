27b036c6805fa2d2f1ac32086d34e9588698d628
Dmitry Petlya <dmitry.petlya@dataart.com>
2018-02-19 13:37:19 +0200

    DEAL-4147

--- Client/DealnetPortal.Web/Scripts/pages/leads/leads.js
+++ Client/DealnetPortal.Web/Scripts/pages/leads/leads.js
@@ -150,19 +150,6 @@ function showTable () {
 
 };
 
-
-$.fn.dataTable.Api.register('order.neutral()', function () {
-    return this.iterator('table', function (s) {
-        s.aaSorting.length = 0;
-        s.aiDisplay.sort(function (a, b) {
-            return a - b;
-        });
-        s.aiDisplayMaster.sort(function (a, b) {
-            return a - b;
-        });
-    });
-});
-
 $.fn.dataTable.ext.search.push(
     function () {
         var pCE = $("#postal-code");
