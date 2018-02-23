91e5e20c9908198bb8cb1bee847c1b3bf3984589
Maccabee0 <marcus.brinda@dataart.com>
2018-02-23 08:20:17 +0200

    DEAL-4203

--- Client/DealnetPortal.Web/App_Start/BundleConfig.cs
+++ Client/DealnetPortal.Web/App_Start/BundleConfig.cs
@@ -131,6 +131,7 @@ namespace DealnetPortal.Web
                 .Include("~/Scripts/utils/navigateToStep.js")
                 .Include("~/Scripts/utils/loader.js")
                 .Include("~/Scripts/utils/financial-functions.clarity.module.js")
+                .Include("~/Scripts/utils/setEqualHeightRows.js")
                 .Include("~/Scripts/pages/summaryAndConfirmation/summary-and-confirmation.js")
                 .Include("~/Scripts/pages/basicInfo/address-autocomplete.js"));
 
