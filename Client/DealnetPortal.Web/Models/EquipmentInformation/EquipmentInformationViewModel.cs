d0e8035e6f00f4ef3a21022564a7806b798906ab
Maccabee0 <marcus.brinda@dataart.com>
2018-03-12 15:16:06 +0200

    DEAL-4428

--- Client/DealnetPortal.Web/Models/EquipmentInformation/EquipmentInformationViewModel.cs
+++ Client/DealnetPortal.Web/Models/EquipmentInformation/EquipmentInformationViewModel.cs
@@ -51,6 +51,8 @@ namespace DealnetPortal.Web.Models.EquipmentInformation
         [Display(ResourceType = typeof(Resources.Resources), Name = "AdminFee")]
         public double? AdminFee { get; set; }
 
+        public bool? DealerPaysAdminFee { get; set; }
+
         [RegularExpression(@"(^[0]?|(^[1-9]\d{0,11}))([.,][0-9]{1,2})?$", ErrorMessageResourceType = typeof(Resources.Resources), ErrorMessageResourceName = "DownPaymentIncorrectFormat")]
         [Display(ResourceType = typeof(Resources.Resources), Name = "DownPayment")]
         public double? DownPayment { get; set; }
