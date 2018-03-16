79ee8e0a1ecf09c980adbf5d6dda531be7f876f6
Dzmitry Rusak <dmitry.rusak@dataart.com>
2018-03-16 13:50:16 +0100

    DEAL-4355: [Back-end] Print contract code changes

--- Server/DealnetPortal.Api.Integration/Services/PdfFormFields.cs
+++ Server/DealnetPortal.Api.Integration/Services/PdfFormFields.cs
@@ -95,8 +95,8 @@ namespace DealnetPortal.Api.Integration.Services
         public static string TotalRetailPrice = "TotalRetailPrice";
 
         public static string MonthlyPayment = "MonthlyPayment";
-        public static string CustomerRate = "CustomerRate";
-        public static string CustomerRate2 = "CustomerRate2";
+        public static string CustomerRate = "CustomerRate"; //AnnualInterestRate
+        public static string CustomerRate2 = "CustomerRate2";//Annual Percentage Rate
         public static string TotalPayment = "TotalPayment";
         public static string TotalMonthlyPayment = "TotalMonthlyPayment";
         public static string Hst = "HST";
