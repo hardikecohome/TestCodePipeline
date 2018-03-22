06c2783bc6bc06285c54fd8d9a5184e1f77341f1
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2018-03-22 15:05:24 +0300

    added Beacon scripts

---
+++ Server/DealnetPortal.Aspire.Integration/Models/AspireDb/CreditReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealnetPortal.Aspire.Integration.Models.AspireDb
+{
+    public class CreditReport
+    {
+        public string CustomerId { get; set; }
+        public string ContractId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public string CrtdTime { get; set; }
+        public DateTime? LastUpdateTime { get; set; }
+        public string LastUpdateUser { get; set; }
+
+        public string CreditId { get; set; }
+        public int Beacon { get; set; }
+    }
+}
