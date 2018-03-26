34ad7394c7fa5087fe3a924793cb117420fe6cf3
Dzmitry Rusak <dmitry.rusak@dataart.com>
2018-03-26 16:42:23 +0200

    DEAL-4445: [Back-end]

--- Client/DealnetPortal.Web/App_Start/AutoMapperConfig.cs
+++ Client/DealnetPortal.Web/App_Start/AutoMapperConfig.cs
@@ -318,7 +318,8 @@ namespace DealnetPortal.Web.App_Start
                 .ForMember(x => x.RequiredDocuments, d => d.MapFrom(src => src.RequiredDocuments))
                 .ForMember(x => x.AdditionalDocuments, d => d.MapFrom(src => src.AdditionalDocuments))
                 .ForMember(x => x.LeadSource, d => d.Ignore());
-            cfg.CreateMap<TierViewModel, TierDTO>();
+            cfg.CreateMap<TierViewModel, TierDTO>()
+                .ForMember(x => x.PassAdminFee, d => d.MapFrom(src => src.AdditionalDocuments));
             cfg.CreateMap<RateCardViewModel, RateCardDTO>();
 
             cfg.CreateMap<EmploymentInformationViewModel, EmploymentInfoDTO>()
