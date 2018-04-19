08169795dfa206afefc78b9c694983604f94d78e
Maccabee0 <marcus.brinda@dataart.com>
2018-04-19 10:16:48 +0300

    DEAL-4782

--- Client/DealnetPortal.Web/App_Start/AutoMapperConfig.cs
+++ Client/DealnetPortal.Web/App_Start/AutoMapperConfig.cs
@@ -294,6 +294,7 @@ namespace DealnetPortal.Web.App_Start
                 .ForMember(x => x.RateCardId, s => s.MapFrom(d => d.SelectedRateCardId))
                 .ForMember(x => x.IsFeePaidByCutomer, s => s.MapFrom(d => d.Conditions.IsAdminFeePaidByCustomer))
                 .ForMember(x => x.HasExistingAgreements, s => s.MapFrom(d => d.Conditions.HasExistingAgreements))
+                .ForMember(x => x.IsClarityProgram, s => s.MapFrom(d => d.Conditions.IsClarityProgram))
                 .ForMember(x => x.SalesRep, s => s.MapFrom(d => d.SalesRepInformation.SalesRep))
                 .ForMember(x => x.EstimatedInstallationDate, s => s.ResolveUsing(d =>
                 {
