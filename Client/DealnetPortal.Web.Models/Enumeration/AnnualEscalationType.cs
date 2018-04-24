a33fd9ee6cc9403652b812bff9ea5fd609bb5c9c
Dzmitry Rusak <dmitry.rusak@dataart.com>
2018-04-24 12:21:02 +0200

    DEAL-4831: [Back-end] Extend code for provide data to UI

---
+++ Client/DealnetPortal.Web.Models/Enumeration/AnnualEscalationType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealnetPortal.Web.Models.Enumeration
+{
+    public enum AnnualEscalationType
+    {
+        [Display(ResourceType = typeof(Resources.Resources), Name = "Escalation_0")]
+        Escalation0 = 0,
+        [Display(ResourceType = typeof(Resources.Resources), Name = "Escalation_35")]
+        Escalation35 = 1
+    }
+}
