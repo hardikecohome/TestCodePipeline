4abf4120d44b41e4294f1cb8f3fc020731e59594
hardikecohome <HSharma@ecohomefinancial.com>
2018-04-30 12:41:52 -0400

    Production Query only for Production deployment branch
    Production Query only for Production deployment branch

--- Server/DealnetPortal.Api/Queries/SearchCustomerAgreements.sql
+++ Server/DealnetPortal.Api/Queries/SearchCustomerAgreements.sql
@@ -29,26 +29,26 @@ DECLARE @TABLE TABLE (
  SET @CUST_COMPANY = '08' 
  DECLARE SMS_CURSOR CURSOR FOR 
  SELECT ROLECOMP.role_id AS cust_id_num 
- FROM LeasePlusTest.dbo.Entity entt 
- INNER JOIN LeasePlusTest.dbo.Role role ON Entt.oid = role.entt_oid AND role.role_type = 'CUST' 
- INNER JOIN LeasePlusTest.dbo.RoleCompany rolecomp ON role.oid = rolecomp.role_oid 
- INNER JOIN LeasePlusTest.dbo.Customer cust ON cust.oid = role.ref_oid and role.role_type = 'CUST' 
- LEFT JOIN LeasePlusTest.dbo.Entity parent ON parent.oid = entt.parent_oid 
- LEFT JOIN LeasePlusTest.dbo.Role parentrole ON parent.oid = parentrole.entt_oid AND parentrole.role_type = 'CUST' 
- LEFT JOIN LeasePlusTest.dbo.RoleCompany parentrolecomp ON parentrole.oid = parentrolecomp.role_oid 
- LEFT JOIN LeasePlusTest.dbo.Customer parentcust ON parentcust.oid = parentrole.ref_oid and parentrole.role_type = 'CUST' 
- LEFT JOIN LeasePlusTest.dbo.Contact cont ON cont.ref_oid = cust.oid AND cont.ref_type = 'CUST' AND cont.ctex_object_type = 'CUST' AND cont.usage_ind = 'P' 
- LEFT JOIN LeasePlusTest.dbo.IndustryType ityp ON ityp.industry_type = entt.ityp_industry_type 
- LEFT JOIN LeasePlusTest.dbo.LeaseSalesRep slsrep ON slsrep.SlsrepCompanyNum = RoleComp.comp_company_num AND slsrep.SlsrepIdNum = RoleComp.cust_slsrep_id_num 
- LEFT JOIN LeasePlusTest.dbo.Location loca ON loca.ref_oid = Cust.oid AND loca.ref_type = 'CUST' AND loca.location_code = 'DFT' 
- LEFT JOIN LeasePlusTest.dbo.Address addr ON addr.oid = loca.addr_oid 
- LEFT JOIN LeasePlusTest.dbo.AddressXref addx ON addx.addr_oid = addr.oid AND addx.ref_type = 'LOCA' AND addx.usage_ind = 'P' 
- LEFT JOIN LeasePlusTest.dbo.Location btloca ON btloca.ref_oid = Cust.oid AND btloca.ref_type = 'CUST' AND btloca.location_code = 'BIL' 
- LEFT JOIN LeasePlusTest.dbo.Address btaddr ON btaddr.oid = btloca.addr_oid 
- LEFT JOIN LeasePlusTest.dbo.AddressXref btaddx ON btaddx.addr_oid = addr.oid AND btaddx.ref_type = 'LOCA' AND btaddx.xref_type = 'B' AND btaddx.usage_ind = 'S' 
- LEFT JOIN LeasePlusTest.dbo.CompanyType comptype ON comptype.company_type = entt.comptype_company_type 
- LEFT JOIN LeasePlusTest.dbo.DataTable ON table_key = 'PERFORMANCE_STATUS' AND data_value = cust.performance_status 
- LEFT OUTER JOIN LeasePlusTest.dbo.LeaseControl cntrl ON cntrl.CntrlCompanyNum = rolecomp.comp_company_num where entt.fname = @CUST_FIRST_NAME and entt.lname = @CUST_LAST_NAME and 
+ FROM LeasePlus.dbo.Entity entt 
+ INNER JOIN LeasePlus.dbo.Role role ON Entt.oid = role.entt_oid AND role.role_type = 'CUST' 
+ INNER JOIN LeasePlus.dbo.RoleCompany rolecomp ON role.oid = rolecomp.role_oid 
+ INNER JOIN LeasePlus.dbo.Customer cust ON cust.oid = role.ref_oid and role.role_type = 'CUST' 
+ LEFT JOIN LeasePlus.dbo.Entity parent ON parent.oid = entt.parent_oid 
+ LEFT JOIN LeasePlus.dbo.Role parentrole ON parent.oid = parentrole.entt_oid AND parentrole.role_type = 'CUST' 
+ LEFT JOIN LeasePlus.dbo.RoleCompany parentrolecomp ON parentrole.oid = parentrolecomp.role_oid 
+ LEFT JOIN LeasePlus.dbo.Customer parentcust ON parentcust.oid = parentrole.ref_oid and parentrole.role_type = 'CUST' 
+ LEFT JOIN LeasePlus.dbo.Contact cont ON cont.ref_oid = cust.oid AND cont.ref_type = 'CUST' AND cont.ctex_object_type = 'CUST' AND cont.usage_ind = 'P' 
+ LEFT JOIN LeasePlus.dbo.IndustryType ityp ON ityp.industry_type = entt.ityp_industry_type 
+ LEFT JOIN LeasePlus.dbo.LeaseSalesRep slsrep ON slsrep.SlsrepCompanyNum = RoleComp.comp_company_num AND slsrep.SlsrepIdNum = RoleComp.cust_slsrep_id_num 
+ LEFT JOIN LeasePlus.dbo.Location loca ON loca.ref_oid = Cust.oid AND loca.ref_type = 'CUST' AND loca.location_code = 'DFT' 
+ LEFT JOIN LeasePlus.dbo.Address addr ON addr.oid = loca.addr_oid 
+ LEFT JOIN LeasePlus.dbo.AddressXref addx ON addx.addr_oid = addr.oid AND addx.ref_type = 'LOCA' AND addx.usage_ind = 'P' 
+ LEFT JOIN LeasePlus.dbo.Location btloca ON btloca.ref_oid = Cust.oid AND btloca.ref_type = 'CUST' AND btloca.location_code = 'BIL' 
+ LEFT JOIN LeasePlus.dbo.Address btaddr ON btaddr.oid = btloca.addr_oid 
+ LEFT JOIN LeasePlus.dbo.AddressXref btaddx ON btaddx.addr_oid = addr.oid AND btaddx.ref_type = 'LOCA' AND btaddx.xref_type = 'B' AND btaddx.usage_ind = 'S' 
+ LEFT JOIN LeasePlus.dbo.CompanyType comptype ON comptype.company_type = entt.comptype_company_type 
+ LEFT JOIN LeasePlus.dbo.DataTable ON table_key = 'PERFORMANCE_STATUS' AND data_value = cust.performance_status 
+ LEFT OUTER JOIN LeasePlus.dbo.LeaseControl cntrl ON cntrl.CntrlCompanyNum = rolecomp.comp_company_num where entt.fname = @CUST_FIRST_NAME and entt.lname = @CUST_LAST_NAME and 
  CONVERT(VARCHAR(20), entt.date_of_birth,101) = @CUST_DATE_OF_BIRTH 
  and ROLECOMP.comp_company_num = @CUST_COMPANY 
  order by cust_id_num desc 
@@ -59,13 +59,13 @@ DECLARE @TABLE TABLE (
  insert into @TABLE select lease_num, 
  lease_app_num, lease_book_type, 
  lease_booked_post_date, lease_sign_date,
-  lease_start_date, lease_maturity_date,
+ lease_start_date, lease_maturity_date,
    lease_term, lease_type_desc, lease_type,
     lease_cust_id_num, equip.equip_type, 
        equip.equip_type_desc, equip.equip_active_flag 
-       from LeasePlusTest.dbo.LPlusLeaseVW 
+       from LeasePlus.dbo.LPlusLeaseVW 
        as lease 
-       left join LeasePlusTest.dbo.[LPlusEquipmentVW] as equip 
+       left join LeasePlus.dbo.[LPlusEquipmentVW] as equip 
        on lease.lease_comp_num = equip.equip_comp_num and lease.lease_cust_id_num = equip.equip_cust_id_num and lease.lease_num = equip.equip_lease_num 
        where lease_cust_id_num = @CUST_ID and lease.lease_term_date is null 
        FETCH NEXT FROM SMS_CURSOR INTO @CUST_ID 
@@ -74,3 +74,7 @@ DECLARE @TABLE TABLE (
        DEALLOCATE SMS_CURSOR 
        select * from @TABLE 
        delete from @TABLE
+
+
+
+ 
