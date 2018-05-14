69be1ada94d1e12d30567007c0f11bd0e26dd957
Dzmitry Rusak <dmitry.rusak@dataart.com>
2018-05-14 17:14:55 +0200

    DEAL-4785: [Back-end] Aspire service changes for passing additional data

--- Server/DealnetPortal.Api.Integration/Services/AspireService.cs
+++ Server/DealnetPortal.Api.Integration/Services/AspireService.cs
@@ -1443,7 +1443,7 @@ namespace DealnetPortal.Api.Integration.Services
                             AssetNo = string.IsNullOrEmpty(eq.AssetNumber) ? null : eq.AssetNumber,
                             Quantity = "1",
                             Cost = GetEquipmentCost(contract, eq, bFirstEquipment)?.ToString(CultureInfo.InvariantCulture),
-                            Description = $"{eq.EquipmentSubType.Description} {eq.Description}",
+                            Description = string.IsNullOrEmpty(eq.EquipmentSubType?.Description) ? eq.Description : $"{eq.EquipmentSubType?.Description} {eq.Description}",
                             AssetClass = new AssetClass() { AssetCode = eq.Type },
                             UDFs = GetEquipmentUdfs(contract, eq).ToList()
                         });
