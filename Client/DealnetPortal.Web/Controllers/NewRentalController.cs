f31e082ce9cd59eb9746ad3c8e8ca63703ad1a73
Dmitry Petlya <dmitry.petlya@dataart.com>
2018-05-14 17:44:18 +0300

    DEAL-4720 update conditions

--- Client/DealnetPortal.Web/Controllers/NewRentalController.cs
+++ Client/DealnetPortal.Web/Controllers/NewRentalController.cs
@@ -245,7 +245,7 @@ namespace DealnetPortal.Web.Controllers
         {
             var model = await _contractManager.GetEquipmentInfoAsync(contractId);
             ViewBag.EquipmentTypes = (await _dictionaryServiceAgent.GetEquipmentTypes()).Item1?.OrderBy(x => x.Description).ToList();
-            ViewBag.CardTypes = model.DealerTier?.RateCards?.Where(q => q.CardType != RateCardType.Custom).Select(x => x.CardType).Distinct().ToList();
+            ViewBag.CardTypes = model.DealerTier?.RateCards?.Select(x => x.CardType).Distinct().ToList();
             ViewBag.AmortizationTerm = model.DealerTier?.RateCards?.ConvertToAmortizationSelectList();
             ViewBag.DefferalPeriod = model.DealerTier?.RateCards?.ConvertToDeferralSelectList();
             if(model.DealerTier != null && model.DealerTier.Name == System.Configuration.ConfigurationManager.AppSettings[PortalConstants.ClarityTierNameKey])
