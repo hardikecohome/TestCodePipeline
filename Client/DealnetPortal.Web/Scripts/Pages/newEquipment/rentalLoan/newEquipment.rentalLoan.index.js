8e020d0d66d8d3589eb4fe5841aefa6e6ace827f
Maccabee0 <marcus.brinda@dataart.com>
2018-05-18 18:01:34 +0300

    DEAL-5106

--- Client/DealnetPortal.Web/Scripts/Pages/newEquipment/rentalLoan/newEquipment.rentalLoan.index.js
+++ Client/DealnetPortal.Web/Scripts/Pages/newEquipment/rentalLoan/newEquipment.rentalLoan.index.js
@@ -167,15 +167,17 @@
                     submitRateCard(option);
                 }
             }
-
+            debugger
             if ($(settings.escalationLimitErrorMsgId).is(':visible')) {
                 event.preventDefault();
+                return;
             }
 
             if ($('#sales-rep-types').is(':visible')) {
                 if (!$('#sales-rep-types').find('input[type=checkbox]:checked').length) {
                     event.preventDefault();
                     _toggleSalesRepErrors(true);
+                    return;
                 }
             }
 
@@ -183,6 +185,7 @@
                 if (!$('#agreement-types').find('input[name="Conditions.HasExistingAgreements"]:checked').length) {
                     event.preventDefault();
                     _toggleAgreementErrors(true);
+                    return;
                 }
             }
 
