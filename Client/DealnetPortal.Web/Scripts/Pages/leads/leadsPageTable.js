331327162fa9dc4d0ab393ace3014d6c82d14794
Dmitry Petlya <dmitry.petlya@dataart.com>
2018-05-18 17:22:59 +0300

    add files

---
+++ Client/DealnetPortal.Web/Scripts/Pages/leads/leadsPageTable.js
@@ -0,0 +1,12 @@
+﻿module.exports('table', function(require) {
+
+    var Paginator = require('paginator');
+
+    var LeadsTable = function(list) {
+        this.datePickerOptions = {
+            yearRange: '1900:' + new Date().getFullYear(),
+            minDate: new Date("1900-01-01"),
+            maxDate: new Date(),
+        };
+    }
+})
