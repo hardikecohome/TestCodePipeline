96d29863dcf1aefe2d78b8be93197c75679ac7fd
Maccabee0 <marcus.brinda@dataart.com>
2018-05-23 09:22:18 +0300

    DEAL-4513: DatePicker iOS Datepicker fix, Placeholder styles

--- Client/DealnetPortal.Web/Scripts/common/ko.datepicker.js
+++ Client/DealnetPortal.Web/Scripts/common/ko.datepicker.js
@@ -1,13 +1,12 @@
 ﻿ko.bindingHandlers.datePicker = {
     init: function (element, valueAccessor, allBindingsAccessor) {
-        var assignDatepicker = module.require('datepicker').assignDatepicker;
         var observable = valueAccessor();
         var options = Object.assign({}, allBindingsAccessor().datePickerOptions || {}, {
             onSelect: function (day) {
                 observable(day);
             }
         });
-        var input = assignDatepicker(element, options);
+        var input = $(element).datepicker(options);
 
         observable.subscribe(function (newValue) {
             input.val(newValue);
