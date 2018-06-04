d9b66a0844a7254c311ad511463d5515eb58cac5
Dzmitry Rusak <dmitry.rusak@dataart.com>
2018-06-04 12:37:24 +0200

    DEAL-5230: BAck-end fix

--- Server/DealnetPortal.Api.Integration/Services/MortgageBrokerService.cs
+++ Server/DealnetPortal.Api.Integration/Services/MortgageBrokerService.cs
@@ -232,7 +232,7 @@ namespace DealnetPortal.Api.Integration.Services
                 {
                     contractData.Equipment = new EquipmentInfo()
                     {
-                        NewEquipment = new List<NewEquipment> { new NewEquipment { Type = improvmentType.Type, Description = improvmentType.Description } }
+                        NewEquipment = new List<NewEquipment> { new NewEquipment { Type = improvmentType.Type, Description = improvmentType.Description, Id = improvmentType.Id} }
                     };
                 }
 
