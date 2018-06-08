65fbe5682f055b2f118614948e19af461fe591e6
Maccabee0 <marcus.brinda@dataart.com>
2018-06-08 16:54:10 +0300

    DEAL-5277

--- Client/DealnetPortal.Web/Scripts/pages/myDeals/table.js
+++ Client/DealnetPortal.Web/Scripts/pages/myDeals/table.js
@@ -304,6 +304,13 @@
         };
 
         this.filterList = function () {
+            if (this.sorter()) {
+                var sort = this.sortDdValues[this.sorter()];
+                this.sortDirection(sort.dir);
+                this.sortedColumn(sort.field);
+                this.sorter('');
+            }
+
             var stat = this.status();
             var equip = this.equipment();
             var type = this.agreementType();
