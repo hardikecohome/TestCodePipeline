5745af7ac71073eb19a9548d488234a2944c1696
Maccabee0 <marcus.brinda@dataart.com>
2018-06-11 18:17:24 +0300

    DEAL-5265

--- Client/DealnetPortal.Web/Scripts/pages/homePage/homePageTable.js
+++ Client/DealnetPortal.Web/Scripts/pages/homePage/homePageTable.js
@@ -371,6 +371,23 @@ module.exports('table', function (require) {
         this.sortedList.subscribe(function (newValue) {
             this.pager.list(newValue);
         }, this);
+
+        $('body').on('click', (function (e) {
+            var $el = $(e.target);
+            var shown;
+            if (!$el.is('.table-row-settings-popup') && !$el.parents('.table-row-settings-popup').length && !$el.is('.gear-ico')) {
+                shown = this.list().find(function (item) {
+                    return item.showActions();
+                });
+                shown && shown.showActions(false);
+            }
+            if (!$el.is('.customer-comment-popup') && !$el.parents('.customer-comment-popup').length && !$el.is('.notes-ico')) {
+                shown = this.list().find(function (item) {
+                    return item.showNotes();
+                });
+                shown && shown.showNotes(false);
+            }
+        }).bind(this));
     };
     return HomePageTable;
 });
