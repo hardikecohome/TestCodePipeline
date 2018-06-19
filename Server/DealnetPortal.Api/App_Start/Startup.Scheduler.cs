ec7bc7b120debea8df29f1e47797468b9f31147a
Maksim Kharlamov <Maxim.Kharlamov@dataart.com>
2018-06-19 13:55:48 +0300

    added UnityJobActivator

--- Server/DealnetPortal.Api/App_Start/Startup.Scheduler.cs
+++ Server/DealnetPortal.Api/App_Start/Startup.Scheduler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web.Http.Dependencies;
 using DealnetPortal.Api.BackgroundScheduler;
 using DealnetPortal.Api.Common.Constants;
 using DealnetPortal.Utilities.Configuration;
@@ -10,8 +11,6 @@ namespace DealnetPortal.Api
 {
     public partial class Startup
     {
-        private IBackgroundSchedulerService _backgroundSchedulerService;
-
         public void ConfigurationScheduler(IAppBuilder app)
         {
             try
@@ -19,7 +18,8 @@ namespace DealnetPortal.Api
                 var config = (IAppConfiguration)System.Web.Http.GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(IAppConfiguration))
                     ?? new AppConfiguration(WebConfigSections.AdditionalSections);
                 GlobalConfiguration.Configuration.UseSqlServerStorage("DefaultConnection");
-                GlobalConfiguration.Configuration.UseActivator(new ContainerJobActivator());
+                //GlobalConfiguration.Configuration.UseActivator(new ContainerJobActivator());
+                GlobalConfiguration.Configuration.UseActivator(new UnityJobActivator(System.Web.Http.GlobalConfiguration.Configuration.DependencyResolver));
                 CleanAllJobs();
                 RecurringJob.AddOrUpdate<IBackgroundSchedulerService>(b =>
                         b.CheckExpiredLeads(DateTime.UtcNow, int.Parse(config.GetSetting(WebConfigKeys.LEAD_EXPIREDMINUTES_CONFIG_KEY))),
@@ -55,6 +55,53 @@ namespace DealnetPortal.Api
         public override object ActivateJob(Type type)
         {
             return System.Web.Http.GlobalConfiguration.Configuration.DependencyResolver.GetService(type);
+        }
+    }
+
+    public class UnityJobActivator : JobActivator
+    {
+        private readonly IDependencyResolver _dependencyResolver;
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="T:UnityJobActivator"/> class
+        /// </summary>
+        /// <param name="container">The unity container to be used</param>
+
+        public UnityJobActivator(IDependencyResolver dependencyResolver)
+        {
+            _dependencyResolver = dependencyResolver ?? throw new ArgumentNullException(nameof(dependencyResolver));
+        }
+
+
+        /// <inheritdoc />
+        public override object ActivateJob(Type jobType)
+        {
+            return _dependencyResolver.GetService(jobType);
+        }
+
+        public override JobActivatorScope BeginScope(JobActivatorContext context)
+        {
+            return new UnityScope(_dependencyResolver.BeginScope());
+        }
+
+        class UnityScope : JobActivatorScope
+        {
+            private readonly IDependencyScope _dependencyScope;
+
+            public UnityScope(IDependencyScope dependencyScope)
+            {
+                _dependencyScope = dependencyScope;
+            }
+
+            public override object Resolve(Type type)
+            {
+                return _dependencyScope.GetService(type);
+            }
+
+            public override void DisposeScope()
+            {
+                _dependencyScope.Dispose();
+            }
         }
     }
 
